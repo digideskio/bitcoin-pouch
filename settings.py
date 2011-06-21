21156b2fd73196155a3bc59eb4d7b199f15df23b
Ryan <ryepdx@gmail.com>
2011-06-21 13:26:00 -0700

    Paranoid sessions breaks things. Disabling for now.

--- settings.py
+++ settings.py
@@ -91,7 +91,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'paranoidsessions.ParanoidSessionMiddleware',
+    #'paranoidsessions.ParanoidSessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django_openid.consumer.SessionConsumer',
     'account.middleware.LocaleMiddleware',
